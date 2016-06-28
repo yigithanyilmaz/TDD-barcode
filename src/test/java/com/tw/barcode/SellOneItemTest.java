292fd72a308d824087c9c79151d43f4b39947337
yigithanyilmaz <yyilmaz@thoughtworks.com>
2016-06-28 16:31:47 +0300

    secondItemFound test added

--- src/test/java/com/tw/barcode/SellOneItemTest.java
+++ src/test/java/com/tw/barcode/SellOneItemTest.java
@@ -1,35 +1,68 @@
 package com.tw.barcode;
 
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 
 
 import java.awt.*;
+import java.rmi.UnexpectedException;
 
 public class SellOneItemTest {
     @Test
-    public void sellingItemFound() throws Exception {
+    public void ItemFound() throws Exception {
         final Display display = new Display();
-        final Sale sale = new Sale();
+        final Sale sale = new Sale(display);
 
-        sale.onBarcode("123456");
-        assertEquals("7,45",display.getText());
+        sale.onBarcode("123");
+        assertEquals("7,45", display.getText());
 
 
     }
 
-    private class Sale {
-        public void onBarcode(String barcode){
+    @Test
+    public void secondItemFound() throws Exception {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
 
-        }
+        sale.onBarcode("456");
+        assertEquals("45,78", display.getText());
 
     }
 
     private class Display {
-        public String getText(){
-          return null;
+
+        private String text;
+
+        public void setText(String text) {
+            this.text = text;
         }
+
+        public String getText(){
+            return text;
+        }
+    }
+
+    private class Sale {
+        private Display display;
+
+        public Sale(Display display) {
+            this.display = display;
+        }
+
+        public void onBarcode(String barcode) throws UnexpectedException {
+
+            if ("123".equals(barcode)) {
+                display.setText("7,45");
+            }
+             else {
+                display.setText("45,78");
+            }
+
+
+        }
+
     }
 }
